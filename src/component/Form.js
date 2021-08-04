ad8ed81b7391cbf7dbe2fb24dd2baf001a079b71
ShakibAhmed007 <shakibahmed09112017@gmail.com>
2021-08-04 07:19:52 -0700

    1 file modified

--- src/component/Form.js
+++ src/component/Form.js
@@ -1,11 +1,27 @@
 import React from 'react';
 export default class Form extends React.Component {
+  state = {
+    userName: 'Shakib'
+  };
+
+  handleUserName = e => {
+    console.log(e.target.value);
+    this.setState({
+      userName: e.target.value
+    });
+  };
+
   render() {
     return (
       <div>
         <form>
           <label>User Name: </label>
-          <input type="text" placeholder="Please enter your name" />
+          <input
+            type="text"
+            placeholder="Please enter your name"
+            value={this.state.userName}
+            onChange={this.handleUserName}
+          />
         </form>
       </div>
     );
